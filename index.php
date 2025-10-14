a7491cfaef6f17d835b87a75c838156a3f7fd601
luvlilii13 <styysolihah13@gmail.com>
2025-10-14 09:36:06 +0700

    test

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
                     <p class="mt-5 text-base mb-7">
                         Nikmati suasana ala Jepang di Arunika Eatery Kuningan menjadi lebih lengkap dengan menyewa kostum Kimono yang bisa membuat kamu merasa beneran di Jepang. Ciptakan dan abadikan moment indah kamu bersama keluarga di berbagai spot foto menarik yang ada di Arunika Eatery Kuningan.
                     </p>
-                    <a href="main/form.php" class="py-3 px-6 bg-red-800 rounded-xl text-white text-lg font-semibold hover:bg-red-950 transition-all ease-in-out duration-200">
+                    <a href="login.php" class="py-3 px-6 bg-red-800 rounded-xl text-white text-lg font-semibold hover:bg-red-950 transition-all ease-in-out duration-200">
                         Sewa Kostum Sekarang
                     </a>
                 </div>
